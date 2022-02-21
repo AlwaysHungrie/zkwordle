2dc720bd00b34083a2d513b3b0c1ce52d389d148
Dhairya-CreatorOS <dhairya@creatoros.co>
2022-02-21 11:08:48 +0530

    hash router

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,35 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { HashRouter as BrowserRouter } from "react-router-dom";
+import { HashRouter, BrowserRouter } from 'react-router-dom';
+
+function withBaseFix(HashRouter: any) {
+  return class extends React.Component {
+    baseElement: HTMLBaseElement;
+    baseHref: string | null;
+    constructor() {
+      super({});
+      this.baseHref = null;
+      this.baseElement = document.querySelector('base') as HTMLBaseElement;
+      if (this.baseElement) {
+        this.baseHref = this.baseElement.getAttribute('href');
+        this.baseElement.setAttribute('href', '');
+      }
+    }
+
+    render() {
+      return <HashRouter {...this.props}>{this.props.children}</HashRouter>;
+    }
+
+    componentDidMount() {
+      if (this.baseElement && this.baseHref) {
+        this.baseElement.setAttribute('href', this.baseHref);
+      }
+    }
+  };
+}
+
+const FixedHashRouter = withBaseFix(HashRouter);
 
 ReactDOM.render(
   <React.StrictMode>
