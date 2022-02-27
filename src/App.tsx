9f50df39e49ddd266bb7877db17d38e6a2429b95
Dhairya-CreatorOS <dhairya@creatoros.co>
2022-02-27 19:56:56 +0530

    hashrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,42 @@
-import * as React from "react";
-import AuthProvider from "./auth/auth";
-import ProtectedPage from "./pages/ProtectedPage";
-import PublicPage from "./pages/PublicPage";
-import {
-  Routes, Route
-} from 'react-router-dom'
-import Layout from "./components/layout";
-import RequireAuth from "./auth/requireAuth";
-import LoginPage from "./pages/LoginPage";
-import GamePage from "./pages/GamePage";
+import * as React from 'react';
+import AuthProvider from './auth/auth';
+import ProtectedPage from './pages/ProtectedPage';
+import PublicPage from './pages/PublicPage';
+import { Routes, Route, BrowserRouter, HashRouter } from 'react-router-dom';
+import Layout from './components/layout';
+import RequireAuth from './auth/requireAuth';
+import LoginPage from './pages/LoginPage';
+import GamePage from './pages/GamePage';
+import { createHashHistory } from 'history';
 
 export default function App() {
+  const history = createHashHistory();
   return (
-    <AuthProvider>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<PublicPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route
-            path="/protected"
-            element={
-              <RequireAuth>
-                <ProtectedPage />
-              </RequireAuth>
-            }
-          />
-          <Route
-            path="/game"
-            element={
-              <RequireAuth>
-                <GamePage />
-              </RequireAuth>
-            }
-          />
-        </Route>
-      </Routes>
-    </AuthProvider>
+    <HashRouter basename='/'>
+      <AuthProvider>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<PublicPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route
+              path="/protected"
+              element={
+                <RequireAuth>
+                  <ProtectedPage />
+                </RequireAuth>
+              }
+            />
+            <Route
+              path="/game"
+              element={
+                <RequireAuth>
+                  <GamePage />
+                </RequireAuth>
+              }
+            />
+          </Route>
+        </Routes>
+      </AuthProvider>
+    </HashRouter>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,41 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { HashRouter, BrowserRouter } from 'react-router-dom';
-
-function withBaseFix(HashRouter: any) {
-  return class extends React.Component {
-    baseElement: HTMLBaseElement;
-    baseHref: string | null;
-    constructor() {
-      super({});
-      this.baseHref = null;
-      this.baseElement = document.querySelector('base') as HTMLBaseElement;
-      if (this.baseElement) {
-        this.baseHref = this.baseElement.getAttribute('href');
-        this.baseElement.setAttribute('href', '');
-      }
-    }
-
-    render() {
-      return <HashRouter {...this.props}>{this.props.children}</HashRouter>;
-    }
-
-    componentDidMount() {
-      if (this.baseElement && this.baseHref) {
-        this.baseElement.setAttribute('href', this.baseHref);
-      }
-    }
-  };
-}
-
-const FixedHashRouter = withBaseFix(HashRouter);
+import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
